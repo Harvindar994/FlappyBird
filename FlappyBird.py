60ef3ca9dc22210e44e448bef66156ee5b7c9810
Harvindar Singh <41161615+Harvindar994@users.noreply.github.com>
2021-07-01 21:04:30 +0530

    Loading Background image of message Box

--- FlappyBird.py
+++ FlappyBird.py
@@ -1172,3 +1172,4 @@ Creating msg box to show msg on screen
 """
 def msg_box(msg):
     global PYIMG_BLACK_COVER, GameWindow, IMG_MSG_BOX
+    background_img = os.path.join(DIR_TEMP_DATA, "temp_bkImage.png")
