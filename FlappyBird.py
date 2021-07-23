c5eb6137f70a848a76557f3ba203125f1dd00421
Harvindar Singh <41161615+Harvindar994@users.noreply.github.com>
2021-07-24 02:03:53 +0530

    Now loading all the component of about session

--- FlappyBird.py
+++ FlappyBird.py
@@ -1894,3 +1894,8 @@ class GameMenu:
         self.ResetBgChangeTime = True
         self.CloseMenu = True
         self.timeList = [0.1, 0.2, 0.3, 0.4, 0.5]
+
+        # component of about session
+        self.loading_about_components = True
+        font_size = 20
+        font_style = Font_sofiapro_light
